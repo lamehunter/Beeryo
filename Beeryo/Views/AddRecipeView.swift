d6b93ce8404388ec0a3a862d4f3faf1ade2d3e44
Nieswieze <lamehunter@gmail.com>
2021-08-29 20:35:26 +0200

    Core data test point

---
+++ Beeryo/Views/AddRecipeView.swift
@@ -0,0 +1,33 @@
+//
+//  AddRecipeView.swift
+//  Beeryo
+//
+//  Created by Jacek K on 29/08/2021.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+  @Environment(\.managedObjectContext) private var viewContext
+  @State var recipeName: String
+  
+    var body: some View {
+      VStack{
+        TextField("Beer Name", text: $recipeName)
+        
+        Button(action: {
+          RecipeEntity.createWith(recipeName: recipeName, using: viewContext)
+        }) {
+          Text("button TExt")
+        }
+        
+      }
+      
+    }
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+      AddRecipeView(recipeName: "beername")
+    }
+}
